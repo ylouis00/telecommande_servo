00562f131d2f787c2df13ae8025c439d0e4f27b3
ylouis00 <95042331+ylouis00@users.noreply.github.com>
2021-11-26 12:00:10 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 radio.setGroup(1)
 basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.isGesture(Gesture.TiltRight)) {
         radio.sendNumber(1)
-    } else if (input.buttonIsPressed(Button.B)) {
-        radio.sendNumber(2)
-    } else if (input.buttonIsPressed(Button.AB)) {
-        radio.sendNumber(3)
-    } else if (input.logoIsPressed()) {
-        radio.sendNumber(4)
     } else if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(2)
+    } else if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(3)
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        radio.sendNumber(4)
+    } else if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(5)
-    } else if (input.isGesture(Gesture.TiltRight)) {
+    } else if (input.buttonIsPressed(Button.B)) {
         radio.sendNumber(6)
     } else {
         radio.sendNumber(0)
